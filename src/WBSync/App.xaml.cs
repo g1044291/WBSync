79e2db7f4d22392cdf90ccb66119760decf68cc4
g1044291 <g1044291@gmail.com>
2026-06-07 16:15:26 +0900

    change 初期表示サイズと位置を修正

--- src/WBSync/App.xaml.cs
+++ src/WBSync/App.xaml.cs
@@ -9,7 +9,21 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new MainPage()) { Title = "WBSync" };
+            const int width = 1200;
+            const int height = 650;
+
+            var info = DeviceDisplay.Current.MainDisplayInfo;
+            var screenW = info.Width / info.Density;
+            var screenH = info.Height / info.Density;
+
+            return new Window(new MainPage())
+            {
+                Title = "WBSync",
+                Width = width,
+                Height = height,
+                X = (screenW - width) / 2,
+                Y = (screenH - height) / 2,
+            };
         }
     }
 }
